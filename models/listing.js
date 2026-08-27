0ef34a8811c97d9d8ad94ba662063c8813e0e75b
AjeetGiri01 <giriajeet543@gmail.com>
2026-08-27 21:33:07 +0530

    created the index and show route of the project

--- models/listing.js
+++ models/listing.js
@@ -6,7 +6,7 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-    discription: String,
+    description: String,
     image: {
         type: String,
         default: "https://images.unsplash.com/photo-1604863884646-a78d926af041?q=80&w=1305&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
