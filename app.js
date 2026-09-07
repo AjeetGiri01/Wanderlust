74e28f84aa0452525ab951719a737e7026665243
AjeetGiri01 <giriajeet543@gmail.com>
2026-09-08 00:23:19 +0530

    adding more error handling and the validation for schema

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 main().then(() => {
         console.log("connected to DB");
@@ -28,6 +29,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
+
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
 
 //index route
 app.get(
@@ -56,10 +67,8 @@ app.get(
 //create route
 app.post(
     "/listings", 
+    validateListing,
     wrapAsync(async (req, res, next) => {
-        if(!req.body.listing) {
-            throw new ExpressError(400, "Send valid data for listing");
-        }
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
@@ -79,10 +88,8 @@ app.get(
 //update route
 app.put(
     "/listings/:id", 
+    validateListing,
     wrapAsync(async (req, res) => {
-        if(!req.body.listing) {
-            throw new ExpressError(400, "Send valid data for listing");
-        }
         let { id } = req.params;
         await Listing.findByIdAndUpdate(id, {...req.body.listing});
         res.redirect(`/listings/${id}`);
@@ -119,7 +126,9 @@ app.all("/*splat", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let {statusCode=500, message="Something went wrong"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { message });
+    
+    // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
